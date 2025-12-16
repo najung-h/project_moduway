b2bbcca4f3240821bdf25c2ae20df49367c063bb
DESKTOP-FAPRJQS\hg <han970112@gmail.com>
2025-12-17 01:32:47 +0900

    chore(setup): Vue 프론트엔드 초기 환경 및 앱 디렉토리 설정 - Vue3 프로젝트 생성 - 컴포넌트 구조 설정 - 컴포넌트별 역할 주석 작성

---
+++ frontend/project-moduway/src/App.vue
@@ -0,0 +1,12 @@
+// 최상위 루트 컴포넌트로, 앱의 전체적인 레이아웃과 라우터 뷰를 관리합니다.
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
