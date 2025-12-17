bacbfba05fa20a79bc619c5651beb04ae1c7d5ef
DESKTOP-bd <han970112@gmail.com>
2025-12-18 00:31:15 +0900

    feat(L02): 강의 검색페이지 추가 - 검색 기능 미구현(ES 도입 예정) - coursecard 컴포넌트 재사용(메인페이지 coursecard와 구분시 새 컴포넌트 필요) - 강의 목록은 목업 데이터(DB 연동 후 추가 예정)

--- frontend/project-moduway/src/router/index.js
+++ frontend/project-moduway/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexPage from '@/pages/IndexPage.vue'
+import CourseListPage from '@/pages/CourseListPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: IndexPage,
+    },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: CourseListPage,
     },
     // 다른 라우트들은 작업하면서 추가
   ],
